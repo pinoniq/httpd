9804062d9eea0ee4b10a6324506ba69dac75bc42
Jeroen Meeus <jme@emakina.com>
2017-06-30 09:22:29 +0200

    Removed flavors and made it freely configurable

--- entrypoint/httpd-setup.sh
+++ entrypoint/httpd-setup.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-block_symfony="
+vhost="
 <VirtualHost *:80>
     ServerName $HOSTNAME
     ServerAlias *.$HOSTNAME
 
-    DocumentRoot \"/var/www/web\"
+    DocumentRoot \"/var/www/$DOCUMENT_ROOT\"
 
     EnableSendfile off
 
-    DirectoryIndex app.php app_dev.php
+    DirectoryIndex $INDEX
 
     SetEnv ENVIRONMENT $ENVIRONMENT
 
@@ -25,56 +25,5 @@ block_symfony="
 </VirtualHost>
 "
 
-block_drupal="
-<VirtualHost *:80>
-    ServerName $HOSTNAME
-    ServerAlias *.$HOSTNAME
-
-    DocumentRoot \"/var/www\"
-
-    EnableSendfile off
-
-    DirectoryIndex index.php
-
-    SetEnv ENVIRONMENT $ENVIRONMENT
-
-    <Directory /var/www>
-        Options FollowSymlinks
-        AllowOverride All
-        Require all granted
-    </Directory>
-
-    <FilesMatch \.php$>
-        SetHandler \"proxy:fcgi://php:9000\"
-    </FilesMatch>
-</VirtualHost>
-"
-
-block_laravel="
-<VirtualHost *:80>
-    ServerName $HOSTNAME
-    ServerAlias *.$HOSTNAME
-
-    DocumentRoot \"/var/www/public\"
-
-    EnableSendfile off
-
-    DirectoryIndex index.php
-
-    SetEnv ENVIRONMENT $ENVIRONMENT
-
-    <Directory /var/www>
-        Options FollowSymlinks
-        AllowOverride All
-        Require all granted
-    </Directory>
-
-    <FilesMatch \.php$>
-        SetHandler \"proxy:fcgi://php:9000\"
-    </FilesMatch>
-</VirtualHost>
-"
-
-vhost="block_$FLAVOR"
-echo "${!vhost}" > "/usr/local/apache2/conf/extra/httpd-vhosts.conf"
+echo "$vhost" > "/usr/local/apache2/conf/extra/httpd-vhosts.conf"
 httpd-foreground
